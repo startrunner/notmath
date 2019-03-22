95a956658fb3bef010b525b5cd669def2b7a2bb0
Svetlana Teneva <steneva99@gmail.com>
2019-03-22 23:26:24 +0200

    Add `NotationBase` and `IFocusHost`.

---
+++ Mathematica/Contracts/IFocusHost.cs
@@ -0,0 +1,13 @@
+﻿namespace Mathematica.Contracts
+{
+	public interface IFocusHost
+	{
+		bool FocusFirst();
+
+		bool FocusLast();
+
+		bool FocusNext();
+
+		bool FocusPrevious();
+	}
+}
---
+++ Mathematica/Controls/NotationBase.cs
@@ -0,0 +1,32 @@
+﻿using Mathematica.Contracts;
+using System.Windows;
+using System.Windows.Controls;
+
+namespace Mathematica.Controls
+{
+	public abstract class NotationBase : MathBox, IFocusHost
+	{
+		public double FontSizeCoefficient { get; set; }
+
+		protected NotationBase()
+		{
+			Loaded += HandleLoaded;
+		}
+
+		private void HandleLoaded(object sender, RoutedEventArgs e)
+		{
+			base.OnInitialized(e);
+			if (!(Parent is MathBox parent)) return;
+
+			FontSize = FontSizeCoefficient * parent.FontSize;
+		}
+
+		public abstract bool FocusFirst();
+
+		public abstract bool FocusLast();
+
+		public abstract bool FocusNext();
+
+		public abstract bool FocusPrevious();
+	}
+}
