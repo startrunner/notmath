7a51abb6893a231b2119424c6955df8b85bc2d8e
Angel Dimitrov <angeldimitrov05@gmail.com>
2019-03-23 20:47:41 +0200

    toolbar

---
+++ Mathematica/Controls/FormatToolbar.xaml.cs
@@ -0,0 +1,18 @@
+﻿using System.Diagnostics;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Media;
+
+namespace Mathematica.Controls
+{
+    /// <summary>
+    /// Interaction logic for FormatToolbar.xaml
+    /// </summary>
+    public partial class FormatToolbar : UserControl
+    {
+        public FormatToolbar()
+        {
+            InitializeComponent();
+        }
+    }
+}
