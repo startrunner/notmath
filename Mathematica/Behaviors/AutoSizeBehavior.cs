80193a71294ab41e8815d1744d94faeca5e6dc6c
Alexander Ivanov <alexander_ivanov@mail.com>
2019-04-02 22:49:04 +0300

    fixed vertical spacing for super and subscripts and refactored autosize behavior

--- Mathematica/Behaviors/AutoSizeBehavior.cs
+++ Mathematica/Behaviors/AutoSizeBehavior.cs
@@ -1,5 +1,4 @@
 ﻿using Mathematica.Controls;
-using System;
 using System.Windows;
 using System.Windows.Controls;
 using System.Windows.Documents;
@@ -8,19 +7,45 @@ namespace Mathematica.Behaviors
 {
     public static class AutoSizeBehavior
     {
-        public static readonly DependencyProperty EnableAutoSizeProperty =
-            DependencyProperty.RegisterAttached("EnableAutoSize", typeof(bool),
-                typeof(AutoSizeBehavior), new PropertyMetadata(false, OnEnableAutoSizeChanged));
+        const double ManyMuch = 99999;
 
         private static void OnEnableAutoSizeChanged(DependencyObject d, DependencyPropertyChangedEventArgs e)
         {
             var mathBox = d as MathBox;
             if (mathBox == null) return;
-            RemovePagePadding(mathBox);
-            mathBox.Loaded += (s, _) => RemovePagePadding(s as MathBox);
+
+            if (mathBox.EnableAutoSize)
+            {
+                PrepareMathBox(mathBox);
+                AttachEvents(mathBox);
+            }
+            else
+            {
+                DetachEvents(mathBox);
+            }
+        }
+
+        private static void AttachEvents(MathBox mathBox)
+        {
+            mathBox.Loaded += HandleLoaded;
             mathBox.TextChanged += HandleTextChanged;
             mathBox.SizeChanged += HandleSizeChanged;
             AttachResized(mathBox, HandleResized);
+        }
+
+        private static void DetachEvents(MathBox mathBox)
+        {
+            mathBox.Loaded -= HandleLoaded;
+            mathBox.TextChanged -= HandleTextChanged;
+            mathBox.SizeChanged -= HandleSizeChanged;
+            DetachResized(mathBox, HandleResized);
+        }
+
+        private static void HandleLoaded(object sender, RoutedEventArgs e)
+        {
+            var mathBox = sender as MathBox;
+            if (mathBox == null) return;
+            PrepareMathBox(mathBox);
         }
 
         private static void HandleSizeChanged(object sender, SizeChangedEventArgs e)
@@ -32,43 +57,49 @@ namespace Mathematica.Behaviors
         private static void HandleResized(object sender, RoutedEventArgs e)
         {
             if (sender == e.OriginalSource) return;
-            var mathBox = sender as MathBox;
-            Resize(mathBox);
+            if (sender is MathBox mathBox)
+                Resize(sender as MathBox);
         }
 
         private static void HandleTextChanged(object sender, TextChangedEventArgs e)
         {
-            if (sender == e.OriginalSource)
-            {
+            if (sender != e.OriginalSource) return;
+            if (sender is MathBox mathBox)
                 Resize(sender as MathBox);
-            }
         }
 
-        private static void RemovePagePadding(MathBox mathBox) {
+        private static void PrepareMathBox(MathBox mathBox)
+        {
             mathBox.Document.PagePadding = new Thickness(0);
-            mathBox.Document.PageWidth = 1000;
-            mathBox.HorizontalScrollBarVisibility = System.Windows.Controls.ScrollBarVisibility.Disabled;
+            mathBox.Document.PageWidth = ManyMuch;
+            mathBox.HorizontalScrollBarVisibility = ScrollBarVisibility.Disabled;
         }
+
         public static void Resize(this MathBox mathBox)
         {
-            mathBox.Width = 9999;
-            mathBox.Arrange(new Rect(0, 0, 9999, 9999));
-            Rect firstRect = mathBox.Document.ContentStart.GetCharacterRect(LogicalDirection.Forward);
+            if (mathBox.Visibility != Visibility.Visible) return;
+
+            mathBox.Width = ManyMuch;
+            mathBox.Arrange(new Rect(0, 0, ManyMuch, ManyMuch));
+
             Rect lastRect = mathBox.Document.ContentEnd.GetCharacterRect(LogicalDirection.Forward);
+            if (lastRect == Rect.Empty) return;
             mathBox.Width = lastRect.Right + mathBox.Document.PagePadding.Left + mathBox.Document.PagePadding.Right;
         }
 
         public static readonly RoutedEvent ResizedEvent =
             EventManager.RegisterRoutedEvent("Resized", RoutingStrategy.Bubble, typeof(RoutedEventHandler), typeof(MathBox));
-
         private static void AttachResized(MathBox mathBox, RoutedEventHandler handler)
         {
             mathBox.AddHandler(ResizedEvent, handler);
         }
-
         private static void DetachResized(MathBox mathBox, RoutedEventHandler handler)
         {
             mathBox.RemoveHandler(ResizedEvent, handler);
         }
+
+        public static readonly DependencyProperty EnableAutoSizeProperty =
+            DependencyProperty.RegisterAttached("EnableAutoSize", typeof(bool),
+                typeof(AutoSizeBehavior), new PropertyMetadata(false, OnEnableAutoSizeChanged));
     }
 }
